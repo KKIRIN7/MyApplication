c21c7b2e27a1875e778c36a7b25e4ba247ab1b49
Daiki <sdpenginsdpengin@icloud.com>
2019-12-23 17:54:56 +0900

    テストとかしたよ

--- app/src/main/java/com/example/myapplication/TestActivity.java
+++ app/src/main/java/com/example/myapplication/TestActivity.java
@@ -1,18 +1,17 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MenuActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_test);
     }
     public void onClick0(View v) {
         Intent intent = new Intent(this, ShopActivity.class);
