950004e201fe9d715041b78f7dc27143be3b9045
nishimoriwataru <210352y@ugs.kochi^tech>
2019-12-23 16:32:26 +0900

    ihiuhu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView myText = findViewById(R.id.textView);
-        myText.setText("こちは");
+        myText.setText("こ ちは");
 
         final Button button = findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
