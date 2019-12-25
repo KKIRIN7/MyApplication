b2f370dbd7de8381d8e7a6701483bc2ada03e31a
yuuMA2 <210376g@ugs.kochi-tech.ac.jp>
2019-12-25 22:50:16 +0900

    追加できていなかった、ショップ画面と計測画面を追加

---
+++ app/src/main/java/com/example/myapplication/ErrorInputTrainNameActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ErrorInputTrainNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error_input_train_name);
+    }
+}
