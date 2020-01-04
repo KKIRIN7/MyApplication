9947986962498ac9f21de9d141cd59d74a15bbb7
yuuMA2 <210376g@ugs.kochi-tech.ac.jp>
2020-01-05 01:40:02 +0900

    引継ぎ画面を追加しました。

---
+++ app/src/main/java/com/example/myapplication/HikitugiActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HikitugiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hikitugi);
+    }
+    public void onClick0(View v) {
+        Intent intent = new Intent(this, hikitugiErrorActivity.class);
+        startActivity(intent);
+    }
+    public void onClick1(View v) {
+        Intent intent = new Intent(this, startActivity.class);
+        startActivity(intent);
+    }
+}
