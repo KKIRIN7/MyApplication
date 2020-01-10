99d20645081f7516c09d69f926033368ec612d1e
nobuhara <taka1108@kind.ocn.ne.jp>
2020-01-10 15:17:21 +0900

    DB動作のサンプルとDBを反映したカレンダーです

--- app/src/main/java/com/example/myapplication/CalenderActivity.java
+++ app/src/main/java/com/example/myapplication/CalenderActivity.java
@@ -4,16 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class CalenderActivity extends AppCompatActivity {
     //部品の変数
     EditText showDate;
+    private TextView textView;
+    private EditText editTextKey, editTextValue;
+    private MyOpenHelper helper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +47,7 @@ public class CalenderActivity extends AppCompatActivity {
                             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                                 //setした日付を取得して表示
                                 showDate.setText(String.format("%d / %02d / %02d", year, month+1, dayOfMonth));
+                                searchData(db, year, month+1, dayOfMonth);
                             }
                         },
                         date.get(Calendar.YEAR),
@@ -51,6 +61,47 @@ public class CalenderActivity extends AppCompatActivity {
             }
         });
 
+        editTextKey = findViewById(R.id.edit_text_key);
+        editTextValue = findViewById(R.id.edit_text_value);
+
+        textView = findViewById(R.id.text_view);
+
+        if(helper == null){
+            helper = new MyOpenHelper(getApplicationContext());
+        }
+
+        if(db == null){
+            db = helper.getReadableDatabase();
+        }
+        Log.d("debug","**********Cursor");
+
+        Cursor cursor = db.query(
+                "trainrecorddb",
+                new String[] { "trainname", "setnum" },
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        cursor.moveToFirst();
+
+        StringBuilder sbuilder = new StringBuilder();
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+            sbuilder.append(cursor.getString(0));
+            sbuilder.append("　　");
+            sbuilder.append(cursor.getInt(1));
+            sbuilder.append("\n");
+            cursor.moveToNext();
+        }
+
+        // 忘れずに！
+        cursor.close();
+
+        Log.d("debug","**********"+sbuilder.toString());
+        textView.setText(sbuilder.toString());
     }
 
     public void onClick0(View v) {
@@ -58,4 +109,49 @@ public class CalenderActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void onClick1(View v) {
+        Intent intent = new Intent(this, DBActivity.class);
+        startActivity(intent);
+    }
+
+
+    private void searchData(SQLiteDatabase db, int year, int month, int dayOfMonth) {
+        if(helper == null){
+            helper = new MyOpenHelper(getApplicationContext());
+        }
+
+        if(db == null){
+            db = helper.getReadableDatabase();
+        }
+        Log.d("debug","**********Cursor");
+
+        String key = String.format("%d%02d%02d", year, month, dayOfMonth);
+        Cursor cursor = db.query(
+                "trainrecorddb",
+                new String[] { "trainname", "setnum" },
+                "trainname = \"" + key + "\"",
+                null,
+                null,
+                null,
+                null
+        );
+
+        cursor.moveToFirst();
+
+        StringBuilder sbuilder = new StringBuilder();
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+            sbuilder.append(cursor.getString(0));
+            sbuilder.append("　　");
+            sbuilder.append(cursor.getInt(1));
+            sbuilder.append("\n");
+            cursor.moveToNext();
+        }
+
+        // 忘れずに！
+        cursor.close();
+
+        Log.d("debug","**********"+sbuilder.toString());
+        textView.setText(sbuilder.toString());
+    }
 }
---
+++ app/src/main/java/com/example/myapplication/DBActivity.java
@@ -0,0 +1,182 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class DBActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editTextKey, editTextValue;
+    private MyOpenHelper helper;
+    private SQLiteDatabase db;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_db);
+
+        editTextKey = findViewById(R.id.edit_text_key);
+        editTextValue = findViewById(R.id.edit_text_value);
+
+        textView = findViewById(R.id.text_view);
+
+        Button insertButton = findViewById(R.id.button_insert);
+        insertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(helper == null){
+                    helper = new MyOpenHelper(getApplicationContext());
+                }
+
+                if(db == null){
+                    db = helper.getWritableDatabase();
+                }
+
+                String key = editTextKey.getText().toString();
+                String value = editTextValue.getText().toString();
+
+                insertData(db, key, Integer.valueOf(value));
+            }
+        });
+
+        Button readButton = findViewById(R.id.button_read);
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readData();
+            }
+        });
+
+        Button deleteButton = findViewById(R.id.button_delete);
+        deleteButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String key = editTextKey.getText().toString();
+                deleteData(db, key);
+            }
+        });
+
+        Button alldeleteButton = findViewById(R.id.button_alldelete);
+        alldeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alldeleteData();
+            }
+        });
+
+        Button searchButton = findViewById(R.id.button_search);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String key = editTextKey.getText().toString();
+                searchData(db, key);
+            }
+        });
+    }
+
+
+    private void readData(){
+        if(helper == null){
+            helper = new MyOpenHelper(getApplicationContext());
+        }
+
+        if(db == null){
+            db = helper.getReadableDatabase();
+        }
+        Log.d("debug","**********Cursor");
+
+        Cursor cursor = db.query(
+                "trainrecorddb",
+                new String[] { "trainname", "setnum" },
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        cursor.moveToFirst();
+
+        StringBuilder sbuilder = new StringBuilder();
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+            sbuilder.append(cursor.getString(0));
+            sbuilder.append(": ");
+            sbuilder.append(cursor.getInt(1));
+            sbuilder.append("\n");
+            cursor.moveToNext();
+        }
+
+        // 忘れずに！
+        cursor.close();
+
+        Log.d("debug","**********"+sbuilder.toString());
+        textView.setText(sbuilder.toString());
+    }
+
+    private void insertData(SQLiteDatabase db, String com, int price){
+
+        ContentValues values = new ContentValues();
+        values.put("trainname", com);
+        values.put("setnum", price);
+        db.insert("trainrecorddb", null, values);
+    }
+
+    private void alldeleteData(){
+        db.delete("trainrecorddb", null, null);
+    }
+
+    private void deleteData(SQLiteDatabase db, String key){
+        db.delete("trainrecorddb", "trainname =  \"" + key + "\"", null);
+    }
+
+    private void searchData(SQLiteDatabase db, String key) {
+        if(helper == null){
+            helper = new MyOpenHelper(getApplicationContext());
+        }
+
+        if(db == null){
+            db = helper.getReadableDatabase();
+        }
+        Log.d("debug","**********Cursor");
+
+        Cursor cursor = db.query(
+                "trainrecorddb",
+                new String[] { "trainname", "setnum" },
+                "trainname =  \"" + key + "\"",
+                null,
+                null,
+                null,
+                null
+        );
+
+        cursor.moveToFirst();
+
+        StringBuilder sbuilder = new StringBuilder();
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+            sbuilder.append(cursor.getString(0));
+            sbuilder.append(": ");
+            sbuilder.append(cursor.getInt(1));
+            sbuilder.append("\n");
+            cursor.moveToNext();
+        }
+
+        // 忘れずに！
+        cursor.close();
+
+        Log.d("debug","**********"+sbuilder.toString());
+        textView.setText(sbuilder.toString());
+    }
+}
