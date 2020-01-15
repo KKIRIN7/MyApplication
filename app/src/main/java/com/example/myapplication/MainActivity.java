07ead65ea78295181148b8fcc83c8e59f94c15dc
nobuhara <taka1108@kind.ocn.ne.jp>
2020-01-15 14:37:04 +0900

    mainからの画面遷移を改良

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+
+import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void onClick0(View v) {
+        File db = this.getDatabasePath("User.db");
+        if (db.exists()){
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        }else {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         }
     }
+}
