263bcd5bdff65b42270ce3462ad9aef06a979050
Daiki <sdpenginsdpengin@icloud.com>
2020-01-23 19:03:32 +0900

    ああ

--- app/src/main/java/com/example/myapplication/ShopActivity.java
+++ app/src/main/java/com/example/myapplication/ShopActivity.java
@@ -3,19 +3,28 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShopActivity extends AppCompatActivity {
-
+    private AWSconnect4Shop con;
+    private TextView button1;
+    private TextView button2;
+    private TextView button3;
+    private TextView button4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
-    }
-    public void onClick0(View v) {
-        Intent intent = new Intent(this, ShopActivity.class);
-        startActivity(intent);
+        button1 = findViewById(R.id.button6);
+        button2 = findViewById(R.id.button7);
+        button3 = findViewById(R.id.button8);
+        button4 = findViewById(R.id.button9);
+        con = new AWSconnect4Shop(button1,button2,button3,button4);//idをawsconnectに送る
+        String URL = new String("http://13.113.228.107/ShowGoodsMET.php");//接続するphpファイルの決定
+        String Values = new String("a=2");//androidstudioからphpに値を送る文字列(phpにはaと設定しているためa=XXXとする)
+        con.execute(URL, Values);//第一引数にURL、第二引数以降にphpに送りたいものを入れる
     }
 
     public void onClick1(View v) {
@@ -40,5 +49,37 @@ public class ShopActivity extends AppCompatActivity {
     public void onClick5(View v) {
         Intent intent = new Intent(this, MenuActivity.class);
         startActivity(intent);
+    }
+    public void onClick6(View v) {
+        Intent intent = new Intent(this,ShopdummyActivity.class);
+        String source1 = button1.getText().toString();
+        if (!source1.equals("")) {
+            intent.putExtra("source", source1);
+            startActivity(intent);
+        }
+    }
+    public void onClick7(View v) {
+        Intent intent = new Intent(this,ShopdummyActivity.class);
+        String source2 = button2.getText().toString();
+        if (!source2.equals("")) {
+            intent.putExtra("source", source2);
+            startActivity(intent);
+        }
+    }
+    public void onClick8(View v) {
+        Intent intent = new Intent(this,ShopdummyActivity.class);
+        String source3 = button3.getText().toString();
+        if (!source3.equals("")) {
+            intent.putExtra("source", source3);
+            startActivity(intent);
+        }
+    }
+    public void onClick9(View v) {
+        Intent intent = new Intent(this,ShopdummyActivity.class);
+        String source4 = button4.getText().toString();
+        if (!source4.equals("")) {
+            intent.putExtra("source", source4);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/ShopMachineActivity.java
+++ app/src/main/java/com/example/myapplication/ShopMachineActivity.java
@@ -5,13 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class ShopMachineActivity extends AppCompatActivity {
-
+    private AWSconnect4Shop con;
+    private TextView button1;
+    private TextView button2;
+    private TextView button3;
+    private TextView button4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_machine);
+        button1 = findViewById(R.id.button6);
+        button2 = findViewById(R.id.button7);
+        button3 = findViewById(R.id.button8);
+        button4 = findViewById(R.id.button9);
+        con = new AWSconnect4Shop(button1,button2,button3,button4);//idをawsconnectに送る
+        String URL = new String("http://13.113.228.107/ShowGoodsMET.php");//接続するphpファイルの決定
+        String Values = new String("a=1");//androidstudioからphpに値を送る文字列(phpにはaと設定しているためa=XXXとする)
+        con.execute(URL, Values);//第一引数にURL、第二引数以降にphpに送りたいものを入れる
     }
     public void onClick0(View v) {
         Intent intent = new Intent(this, ShopActivity.class);
@@ -20,11 +33,6 @@ public class ShopMachineActivity extends AppCompatActivity {
 
     public void onClick1(View v) {
         Intent intent = new Intent(this, ShopSupplyActivity.class);
-        startActivity(intent);
-    }
-
-    public void onClick2(View v) {
-        Intent intent = new Intent(this, ShopMachineActivity.class);
         startActivity(intent);
     }
 
@@ -40,5 +48,37 @@ public class ShopMachineActivity extends AppCompatActivity {
     public void onClick5(View v) {
         Intent intent = new Intent(this, MenuActivity.class);
         startActivity(intent);
+    }
+    public void onClick6(View v) {
+        Intent intent = new Intent(this,ShopdummyActivity.class);
+        String source1 = button1.getText().toString();
+        if (!source1.equals("")) {
+            intent.putExtra("source", source1);
+            startActivity(intent);
+        }
+    }
+    public void onClick7(View v) {
+        Intent intent = new Intent(this,ShopdummyActivity.class);
+        String source2 = button2.getText().toString();
+        if (!source2.equals("")) {
+            intent.putExtra("source", source2);
+            startActivity(intent);
+        }
+    }
+    public void onClick8(View v) {
+        Intent intent = new Intent(this,ShopdummyActivity.class);
+        String source3 = button3.getText().toString();
+        if (!source3.equals("")) {
+            intent.putExtra("source", source3);
+            startActivity(intent);
+        }
+    }
+    public void onClick9(View v) {
+        Intent intent = new Intent(this,ShopdummyActivity.class);
+        String source4 = button4.getText().toString();
+        if (!source4.equals("")) {
+            intent.putExtra("source", source4);
+            startActivity(intent);
+        }
     }
 }
